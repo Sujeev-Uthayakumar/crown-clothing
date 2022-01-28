abfe19d8c6b41fc50ea2e5864ede5eee0a288d0a
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2022-01-28 01:06:02 -0500

    add styled homepage

---
+++ src/pages/homepage/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Directory from "../../components/directory/Directory";
+import "./HomePage.scss";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default Homepage;
