ffe9a1560cfc2406a35f18da22e1bf5ea9760e93
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2022-02-02 19:26:32 -0500

    updated shop data with new data, updated cart utils to group items in car reducer

--- src/components/item-collection/CollectionItem.js
+++ src/components/item-collection/CollectionItem.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import CustomButton from "../custom-button/CustomButton";
+import { addItem } from "../../redux/cart/cartActions";
 
 import "./CollectionItem.scss";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
   return (
     <div className="collection-item">
       <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -10,8 +15,11 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add to cart
+      </CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+export default connect(null, { addItem })(CollectionItem);
--- src/redux/cart/cartActions.js
+++ src/redux/cart/cartActions.js
@@ -1,3 +1,5 @@
 import cartTypes from "./cartTypes";
 
 export const toggleCartHidden = () => ({ type: cartTypes.TOGGLE_CART_HIDDEN });
+
+export const addItem = (item) => ({ type: cartTypes.ADD_ITEM, payload: item });
