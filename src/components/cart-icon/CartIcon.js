7449113bfdf9f8f70f885d56d3d0e4bf0b765d7c
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2022-02-07 01:54:29 -0500

    add cart-icon quantity function

--- src/components/cart-icon/CartIcon.js
+++ src/components/cart-icon/CartIcon.js
@@ -11,11 +11,21 @@ const CartIcon = (props) => {
   return (
     <div className="cart-icon" onClick={props.toggleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{props.itemCount}</span>
     </div>
   );
 };
 
-export default connect(null, {
+const mapStateToprops = ({ cart: { cartItems } }) => {
+  return {
+    itemCount: cartItems.reduce(
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity,
+      0
+    ),
+  };
+};
+
+export default connect(mapStateToprops, {
   toggleCartHidden,
 })(CartIcon);
