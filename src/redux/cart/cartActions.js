3ad65de4a5a05adfd22daaca25fa3c656801938a
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2022-02-08 14:45:25 -0500

    add the ability to remove items from checkout

--- src/redux/cart/cartActions.js
+++ src/redux/cart/cartActions.js
@@ -1,5 +1,11 @@
+import CartActionTypes from "./cartTypes";
 import cartTypes from "./cartTypes";
 
 export const toggleCartHidden = () => ({ type: cartTypes.TOGGLE_CART_HIDDEN });
 
 export const addItem = (item) => ({ type: cartTypes.ADD_ITEM, payload: item });
+
+export const clearItemFromCart = (item) => ({
+  type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item,
+});
