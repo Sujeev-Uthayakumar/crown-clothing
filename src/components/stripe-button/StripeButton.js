d7a43cdaebf3a0f6ef5bc52195825f551ed6565e
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2022-02-10 10:03:40 -0500

    integrate stripe payments

---
+++ src/components/stripe-button/StripeButton.js
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51KRd3dAnfsqHLtn9lJv1LZCcOvDZXTkxE9ebEALlKRm8YsQCapUSSwL4hcXcalolltoPOVSWP6nZndWfmXkLBzov00haY6hgYG";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CROWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is ${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeButton;
